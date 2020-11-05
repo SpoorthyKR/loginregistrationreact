545846147409d4d4cc5c3a76429910fb60608c63
Spoorthy Rajashekar <57374095+SpoorthyKR@users.noreply.github.com>
2020-11-05 18:55:06 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
+
 injectTapEventPlugin();
 import './App.css';
 import Loginscreen from './Loginscreen'
